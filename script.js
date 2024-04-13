c9d854b502f5e61bf9cc078729deea9324300ac6
Gyanvendra Singh <154894712+gyanvendra2004@users.noreply.github.com>
2024-04-13 23:26:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let progress = document.querySelector('#progress')
+let song = document.querySelector('#audio')
+let control = document.querySelector('#play')
+// console.log(progress);
+// document.getElementById("progress").disabled = true
+// progress.document.style.
+ song.onloadedmetadata = function(){
+    progress.max =song.duration
+    progress.value =song.currentTime
+
+ }
+
+
+ function playPause(){
+     if(control.classList.contains("fa-pause")){
+        song.pause();
+        control.classList.remove("fa-pause")
+        control.classList.add("fa-play")
+     }
+     else{
+        song.play()
+        control.classList.add("fa-pause")
+        control.classList.remove("fa-play")
+     }
+ }
+
+
+ if(song.play()){
+    setInterval(() =>{
+        progress.value = song.currentTime
+    },500)
+ }
+ progress.onchange = function(){
+    song.play()
+    song.currentTime = progress.value
+    control.classList.remove("fa-play")
+    control.classList.add("fa-pause")
+ }
+
+ control.addEventListener('click', () => {
+       playPause()
+       console.log("hi");
+ })
